33371f720028bed7a56b58cf74b7fc9a16ea6083
System Administrator <root@Pavels-MacBook-Pro.local>
2021-04-02 01:12:42 +0300

    Added some templates

--- todo/views.py
+++ todo/views.py
@@ -4,4 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def todo_list(request):
-    return HttpResponse("Hello World")
+    return render(request, "todo/todo_list.html")
+    # return render(request, "todo_list2.html")
+
+# def todo_list(request):
+#     return HttpResponse("Hello World")
