2e07ee97aa083f37ab812796f920de4251b2cb84
System Administrator <root@Pavels-MacBook-Pro.local>
2021-04-12 00:50:27 +0300

    Todo List View

--- crashcourse/urls.py
+++ crashcourse/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from todo.views import todo_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', todo_list)
+    path('', include('todo.urls', namespace='todos'))
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,27 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Todo
+
 # Create your views here.
 
 def todo_list(request):
-    return render(request, "todo/todo_list.html")
-    # return render(request, "todo_list2.html")
+    todos = Todo.objects.all()
+    print(todos)
+    context = {
+        "todo_list": todos
+    }
+    return render(request, "todo/todo_list.html", context)
+
+# def todo_list(request):
+#     todos = Todo.objects.all()
+#     print(todos)
+#     return render(request, "todo/todo_list.html")
+
+
+# def todo_list(request):
+#     return render(request, "todo/todo_list.html")
+#     # return render(request, "todo_list2.html")
 
 # def todo_list(request):
 #     return HttpResponse("Hello World")
